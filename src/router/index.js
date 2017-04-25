f3cc9db152cd1f30ec03a9da031f232ff6489abc
guylil <guylilian@gmail.com>
2017-04-25 17:43:37 +0300

    setting the main components

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Hello from '../components/Hello'
+import settings from '../components/settings'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
-    }
+    },
+    {
+      path: '/settings',
+      name: 'Settings',
+      component: settings
+    },
   ]
 })
